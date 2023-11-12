8bc8b2fd785a5d2cf882d5c67fc370ce424b2266
Robert Nugent <d00244618@student.dkit.ie>
2023-11-12 18:55:21 +0000

    Adjusted styling on mobile login page

--- mobile_application/app/src/main/java/com/example/aerosense_app/MainActivity.kt
+++ mobile_application/app/src/main/java/com/example/aerosense_app/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.ui.Alignment
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,15 +15,26 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -51,8 +63,12 @@ fun App() {
     AppContent()
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppContent() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
     Box(
         modifier = Modifier
             .fillMaxSize(),
@@ -60,7 +76,8 @@ fun AppContent() {
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Top
+            verticalArrangement = Arrangement.Top,
+            modifier = Modifier.padding(start = 16.dp) // Add padding to the outer column
         ) {
             Image(
                 painter = painterResource(id = R.drawable.logo_no_background),
@@ -69,6 +86,7 @@ fun AppContent() {
                     .size(300.dp) // Adjust the size as needed
             )
 
+            // Login-related elements
             Text(
                 text = "Log In",
                 style = TextStyle(
@@ -76,23 +94,114 @@ fun AppContent() {
                     fontWeight = FontWeight.Bold
                 ),
                 modifier = Modifier
+                    .padding(start = 16.dp)
                     .align(Alignment.Start)
-
             )
 
-            //Add some smaller gray text below the Log In text that says "Please sign in to continue"
+            // "Please sign in to continue" text
             Text(
                 text = "Please sign in to continue",
-                //Make the text gray
                 style = TextStyle(
                     color = Color.Gray,
                     fontSize = 20.sp,
                     fontWeight = FontWeight.Bold
                 ),
-                modifier = Modifier.align(Alignment.Start)
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .align(Alignment.Start)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Email Row with image and TextField
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.email),
+                    contentDescription = "Email Icon",
+                    modifier = Modifier.size(35.dp)
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                TextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f), // Take remaining space in the Row
+                    label = { Text("Email") }
+                )
+            }
+
+            // Password Row with image and TextField
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.password),
+                    contentDescription = "Password Icon",
+                    modifier = Modifier.size(35.dp)
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                TextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f), // Take remaining space in the Row
+                    label = { Text("Password") },
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password)
+                )
+            }
+
+// Login Button wrapped in a Row for right alignment
+            Row(
+                horizontalArrangement = Arrangement.End,
+                modifier = Modifier.padding(start = 240.dp)
+            ) {
+                Button(
+                    onClick = {
+                        // Handle login button click
+                    },
+                    modifier = Modifier
+                        .height(53.dp),
+                    shape = MaterialTheme.shapes.large // Apply rounded corners
+                ) {
+                    Text(text = "Log In")
+                }
+            }
+
+            Spacer(modifier = Modifier.height(165.dp))
+
+            //Make text for not having account and click to register
+            Text(
+                text = "Don't have an account?",
+                style = TextStyle(
+                    color = Color.Blue,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .align(Alignment.Start)
             )
 
         }
     }
 }
+
+
+
+
 
