f6fd6dd1a21503c2c296c79cc22d364feb7e3fac
Robert Nugent <d00244618@student.dkit.ie>
2023-11-19 18:18:44 +0000

    Creating initial layout of main data screen

--- mobile_application/app/src/main/java/com/example/aerosense_app/MainActivity.kt
+++ mobile_application/app/src/main/java/com/example/aerosense_app/MainActivity.kt
@@ -1,45 +1,81 @@
 package com.example.aerosense_app
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.ui.Alignment
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import com.example.aerosense_app.ui.theme.AeroSense_AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -62,12 +98,12 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun App() {
-    Register()
+    dataScreen()
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Login(navController: NavController) {
+fun Login() {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
@@ -205,7 +241,7 @@ fun Login(navController: NavController) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Register(navController: NavController){
+fun Register(){
     var name by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
@@ -371,9 +407,6 @@ fun Register(navController: NavController){
                 modifier = Modifier
                     .padding(start = 16.dp)
                     .align(Alignment.Start)
-                    .clickable {
-                        navController.navigate("login")
-                    }
             )
 
         }
@@ -381,6 +414,197 @@ fun Register(navController: NavController){
 
 
 }
+
+@Composable
+fun dataScreen(modifier: Modifier = Modifier) {
+    NavigationBar(modifier) {
+        Box(
+            modifier = Modifier
+                .requiredWidth(width = 718.dp)
+                .requiredHeight(height = 93.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .requiredWidth(width = 718.dp)
+                    .requiredHeight(height = 65.dp)
+                    .clip(shape = RoundedCornerShape(6.dp))
+                    .background(color = Color(0xff0d99ff)))
+            Image(
+                painter = painterResource(id = R.drawable.logo_no_background),
+                contentDescription = "logo-no-background",
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .offset(y = -10.dp)
+                    .requiredWidth(width = 200.dp)
+                    .requiredHeight(height = 50.dp))
+            Image(
+                painter = painterResource(id = R.drawable.hamburger),
+                contentDescription = "Hamburger_icon 2",
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterEnd))
+
+        }
+    }
+
+//    Box(
+//        modifier = modifier
+//            .requiredWidth(width = 320.dp)
+//            .requiredHeight(height = 291.dp)
+//    ) {
+//        Box(
+//            modifier = Modifier
+//                .align(alignment = Alignment.TopStart)
+//                .offset(x = 13.dp,
+//                    y = 19.dp)
+//                .requiredWidth(width = 287.dp)
+//                .requiredHeight(height = 272.dp)
+//                .background(color = Color(0xfff24822))
+//                .border(border = BorderStroke(4.dp, Color(0xffc23a1b))))
+//        Box(
+//            modifier = Modifier
+//                .requiredWidth(width = 320.dp)
+//                .requiredHeight(height = 129.dp)
+//                .clip(shape = RoundedCornerShape(6.dp))
+//                .background(color = Color.White))
+//        Box(
+//            modifier = Modifier
+//                .align(alignment = Alignment.TopStart)
+//                .offset(x = 15.dp,
+//                    y = 19.dp)
+//                .requiredWidth(width = 287.dp)
+//                .requiredHeight(height = 272.dp)
+//                .clip(CircleShape)
+//                .border(border = BorderStroke(4.dp, Color(0xff1e1e1e))))
+//        Text(
+//            text = "  57% Clean",
+//            color = Color(0xff1e1e1e),
+//            lineHeight = 3.75.em,
+//            style = TextStyle(
+//                fontSize = 40.sp,
+//                fontWeight = FontWeight.Medium),
+//            modifier = Modifier
+//
+//                .align(alignment = Alignment.Center))
+//    }
+
+    Box(
+        modifier = modifier
+            .requiredWidth(width = 320.dp)
+            .requiredHeight(height = 291.dp)
+            .offset(y = -180.dp)
+
+    ) {
+        // Use drawBehind to draw a hollow black circle with a border
+        Box(
+            modifier = Modifier
+                .align(alignment = Alignment.TopStart)
+                .offset(x = 15.dp, y = 19.dp)
+                .requiredWidth(width = 287.dp)
+                .requiredHeight(height = 272.dp)
+                .drawBehind {
+                    drawCircle(
+                        color = Color.Black,
+                        center = center,
+                        radius = size.minDimension / 3,
+                        style = Stroke(4.dp.toPx())
+                    )
+                }
+        )
+        Text(
+            text = "57% Clean",
+            color = Color(0xff1e1e1e),
+            lineHeight = 3.75.em,
+            style = TextStyle(
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Medium
+            ),
+            modifier = Modifier
+                .align(alignment = Alignment.Center)
+        )
+    }
+
+    Spacer(modifier = Modifier.height(70.dp))
+
+    Box(
+        modifier = modifier
+            .requiredWidth(width = 534.dp)
+            .requiredHeight(height = 123.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.particles),
+            contentDescription = "download 1",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .align(alignment = Alignment.TopStart)
+                .offset(
+                    x = 100.dp,
+                    y = 4.54547119140625.dp
+                )
+                .requiredWidth(width = 100.dp)
+                .requiredHeight(height = 100.dp))
+        Text(
+            text = "PM2.5 -",
+            color = Color(0xff1e1e1e),
+            lineHeight = 3.75.em,
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium),
+            modifier = Modifier
+                .align(alignment = Alignment.TopStart)
+                .offset(
+                    x = 227.dp,
+                    y = 15.dp
+                ))
+        Text(
+            text = "18µg/m^3",
+            color = Color(0xffffa629),
+            lineHeight = 3.75.em,
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium),
+            modifier = Modifier
+                .align(alignment = Alignment.TopStart)
+                .offset(
+                    x = 320.dp,
+                    y = 15.dp
+                ))
+        Text(
+            text = "PM10 -",
+            color = Color(0xff1e1e1e),
+            lineHeight = 3.75.em,
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium),
+            modifier = Modifier
+                .align(alignment = Alignment.TopStart)
+                .offset(
+                    x = 227.dp,
+                    y = 63.dp
+                ))
+        Text(
+            text = "42 µg/m^3",
+            color = Color(0xffffa629),
+            lineHeight = 3.75.em,
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium),
+            modifier = Modifier
+                .align(alignment = Alignment.TopStart)
+                .offset(
+                    x = 320.dp,
+                    y = 63.dp
+                ))
+    }
+
+
+
+}
+
+
+
+
+
+
 
 
 
