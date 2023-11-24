d2204978be06be3d9baf8b4241f3e4330a7eee78
Patrick Orjieh <orjiehpatrick@gmail.com>
2023-11-24 04:58:16 +0000

    Implement PubNub subscription and database storage
    Added functionality to subscribe to the 'aerosense_channel' on PubNub, added data decryption using the cipher key to ensure secure message handling.

--- backend_hub/run.py
+++ backend_hub/run.py
@@ -1,4 +1,9 @@
 from app import app
+from background_tasks import start_background_task
+
+print("Before starting background task")
+start_background_task(app)
+print("After starting background task")
 
 if __name__ == "__main__":
     app.run(debug=True)
