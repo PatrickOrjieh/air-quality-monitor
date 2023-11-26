9a177a9768a9cb0a58c7534cd7d3e309339bedf6
Patrick Orjieh <orjiehpatrick@gmail.com>
2023-11-26 00:24:48 +0000

    Add Firebase auth and user-hub linkage to /api/home
    This commit message will Implement Firebase ID token validation in /api/home endpoint and Retrieve user and associated hub details using Firebase UID, Retrieve user and associated hub details using Firebase UID.

--- aws_backend_version/app/api/data.py
+++ aws_backend_version/app/api/data.py
@@ -1,12 +1,36 @@
-from flask import jsonify
+from flask import jsonify, request
 from app import app, db
-from app.models import AirQualityMeasurement
+from app.models import User, Hub, AirQualityMeasurement
+import firebase_admin
+from firebase_admin import auth
 from sqlalchemy import desc
 
 @app.route('/api/home', methods=['GET'])
 def get_air_quality_data():
+    # Firebase token verification
+    token = request.headers.get('Authorization')
+    if not token:
+        return jsonify({'error': 'Authorization token is missing'}), 401
+
     try:
-        result = AirQualityMeasurement.query.order_by(desc(AirQualityMeasurement.timestamp)).first()
+        # Verify the Firebase ID token and get the user's information
+        decoded_token = auth.verify_id_token(token)
+        firebase_user_id = decoded_token['uid']
+
+        # Fetch the user associated with the Firebase UID
+        user = User.query.filter_by(firebaseUID=firebase_user_id).first()
+
+        if not user:
+            return jsonify({'error': 'User not found'}), 404
+
+        # Fetch the hub associated with the user
+        hub = Hub.query.filter_by(userID=user.userID).first()
+
+        if not hub:
+            return jsonify({'error': 'No hub associated with this user'}), 404
+
+        # Fetch the latest air quality measurement for the user's hub
+        result = AirQualityMeasurement.query.filter_by(hubID=hub.hubID).order_by(desc(AirQualityMeasurement.timestamp)).first()
 
         if result:
             response_data = {
@@ -19,7 +43,9 @@ def get_air_quality_data():
             }
             return jsonify(response_data), 200
         else:
-            return jsonify({'message': 'No air quality data available.'}), 404
+            return jsonify({'message': 'No air quality data available for this hub.'}), 404
 
+    except firebase_admin.exceptions.FirebaseError:
+        return jsonify({'error': 'Invalid Firebase ID token'}), 401
     except Exception as e:
         return jsonify({'error': str(e)}), 500
--- backend_hub/app/api/data.py
+++ backend_hub/app/api/data.py
@@ -1,13 +1,44 @@
-from flask import jsonify
+from flask import jsonify, request
 from app import app, mysql
+import firebase_admin
+from firebase_admin import auth
 
 @app.route('/api/home', methods=['GET'])
 def get_air_quality_data():
+    # Firebase token verification
+    token = request.headers.get('Authorization')
+    if not token:
+        return jsonify({'error': 'Authorization token is missing'}), 401
+
     try:
+        # Verify the Firebase ID token and get the user's information
+        decoded_token = auth.verify_id_token(token)
+        firebase_user_id = decoded_token['uid']
+
+        # Fetch the user ID associated with the Firebase UID
         cursor = mysql.connection.cursor()
+        cursor.execute('SELECT userID FROM User WHERE firebaseUID = %s', (firebase_user_id,))
+        user_result = cursor.fetchone()
+
+        if not user_result:
+            return jsonify({'error': 'User not found'}), 404
+
+        user_id = user_result[0]
+
+        # Fetch the hub ID associated with the user ID
+        cursor.execute('SELECT hubID FROM Hub WHERE userID = %s', (user_id,))
+        hub_result = cursor.fetchone()
+
+        if not hub_result:
+            return jsonify({'error': 'No hub associated with this user'}), 404
+
+        hub_id = hub_result[0]
+
+        # Fetch the latest air quality measurement for the user's hub
         cursor.execute('''SELECT * FROM AirQualityMeasurement 
+                          WHERE hubID = %s 
                           ORDER BY timestamp DESC 
-                          LIMIT 1''')
+                          LIMIT 1''', (hub_id,))
         row_headers = [x[0] for x in cursor.description]
         result = cursor.fetchone()
         cursor.close()
@@ -24,7 +55,9 @@ def get_air_quality_data():
             }
             return jsonify(response_data), 200
         else:
-            return jsonify({'message': 'No air quality data available.'}), 404
+            return jsonify({'message': 'No air quality data available for this hub.'}), 404
 
+    except firebase_admin.exceptions.FirebaseError:
+        return jsonify({'error': 'Invalid Firebase ID token'}), 401
     except Exception as e:
         return jsonify({'error': str(e)}), 500
