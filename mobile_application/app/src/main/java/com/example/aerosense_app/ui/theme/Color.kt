0c508f851f19063528b851cfa8725b0465244230
Robert Nugent <d00244618@student.dkit.ie>
2024-01-14 20:58:09 +0000

    Adjust some text and titles on settings page

--- mobile_application/app/src/main/java/com/example/aerosense_app/ui/theme/Color.kt
+++ mobile_application/app/src/main/java/com/example/aerosense_app/ui/theme/Color.kt
@@ -6,6 +6,6 @@ val Purple80 = Color(0xFFFFFFFF)
 val PurpleGrey80 = Color(0xFFFFFFFF)
 val Pink80 = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF2596be)
+val Blue40 = Color(0xFF2596be)
 val PurpleGrey40 = Color(0xFFFFFFFF)
 val Pink40 = Color(0xFFFFFFFF)
